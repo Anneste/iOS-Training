db11cc6a03e6cb3aa7db10c59579c96ed9f57ec2
Steven <steven@Stevens-Mac.local>
2012-09-13 15:36:38 +0200

    xcelme

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Steven on 8/31/12.
+//  Copyright (c) 2012 Anneste Design & Development cc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"I'm Back!!");
+        
+    }
+    return 0;
+}
+
